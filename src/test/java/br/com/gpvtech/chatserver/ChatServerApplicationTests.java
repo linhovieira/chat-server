d936a1b901aa97700998be34fa4255bde2ce66ac
Paulo Vieira <linho_017@hotmail.com>
2024-04-27 21:56:45 -0300

    [INIT] - Project base files (direct from Spring Initializr);

---
+++ src/test/java/br/com/gpvtech/chatserver/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gpvtech.chatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
